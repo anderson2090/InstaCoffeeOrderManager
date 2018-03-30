cb3dd3eb22f1aac356f25342061f7d3e3ea8a51a
usama <usamanadeem1991@gmail.com>
2018-03-31 03:33:16 +0500

    created login layout

--- app/src/main/java/com/sweetdeveloper/instacoffeeordermanager/LoginActivity.java
+++ app/src/main/java/com/sweetdeveloper/instacoffeeordermanager/LoginActivity.java
@@ -2,12 +2,72 @@ package com.sweetdeveloper.instacoffeeordermanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @NotEmpty
+    @Email
+    EditText emailEditText;
+
+    @Password
+    EditText passwordEditText;
+
+    Button loginButton;
+    Validator validator;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        loginButton = findViewById(R.id.login_button);
+        progressBar = findViewById(R.id.progress_bar);
+        validator = new Validator(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validator.validate();
+            }
+        });
+
+        validator.setValidationListener(new Validator.ValidationListener() {
+            @Override
+            public void onValidationSucceeded() {
+                Toast.makeText(getApplicationContext(),"Logging in..",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onValidationFailed(List<ValidationError> errors) {
+
+                for (ValidationError error : errors) {
+                    View view = error.getView();
+                    String message = error.getCollatedErrorMessage(getApplicationContext());
+
+                    // Display error messages
+                    if (view instanceof EditText) {
+                        ((EditText) view).setError(message);
+                    } else {
+                        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 }
