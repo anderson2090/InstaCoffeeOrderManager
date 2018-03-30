5db8586739a87347d870c528bf230e59a751ea1e
usama <usamanadeem1991@gmail.com>
2018-03-31 04:24:20 +0500

    created login

--- app/src/main/java/com/sweetdeveloper/instacoffeeordermanager/LoginActivity.java
+++ app/src/main/java/com/sweetdeveloper/instacoffeeordermanager/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.sweetdeveloper.instacoffeeordermanager;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,11 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.Email;
@@ -28,11 +35,21 @@ public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     Validator validator;
     ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+                startActivity(new Intent(getApplicationContext(), OrdersActivity.class));
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.please_verify_email), Toast.LENGTH_SHORT).show();
+            }
+        }
         emailEditText = findViewById(R.id.email_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
         loginButton = findViewById(R.id.login_button);
@@ -49,7 +66,8 @@ public class LoginActivity extends AppCompatActivity {
         validator.setValidationListener(new Validator.ValidationListener() {
             @Override
             public void onValidationSucceeded() {
-                Toast.makeText(getApplicationContext(),"Logging in..",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.VISIBLE);
+                logIn();
             }
 
             @Override
@@ -63,10 +81,39 @@ public class LoginActivity extends AppCompatActivity {
                     if (view instanceof EditText) {
                         ((EditText) view).setError(message);
                     } else {
-                        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
                     }
                 }
 
+            }
+        });
+    }
+
+    public void logIn() {
+
+        firebaseAuth.signInWithEmailAndPassword(emailEditText.getText().toString(),
+                passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressBar.setVisibility(View.GONE);
+                            if (firebaseAuth.getCurrentUser() != null) {
+                                if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+                                    startActivity(new Intent(getApplicationContext(), OrdersActivity.class));
+                                    finish();
+                                } else {
+
+                                    Toast.makeText(getApplicationContext(), getString(R.string.please_verify_email), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
             }
         });
     }
